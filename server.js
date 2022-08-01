9936bfd9ce3e9e8c10cbef5829cbc23f199d6e06
“r4dra” <rudrapratapsinghtomar@gmail.com>
2022-08-01 11:10:36 +0530

    react app created

--- server.js
+++ server.js
@@ -19,4 +19,4 @@ app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 3010;
 
-app.listen(PORT, () => console.log(`Server started on poert ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
