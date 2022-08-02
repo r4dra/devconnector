840f37b2903b19dc7b39c6c81b08dc3aeef16fa5
“r4dra” <rudrapratapsinghtomar@gmail.com>
2022-08-03 03:02:02 +0530

    Redux setup done

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,16 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import Alert from './components/layout/Alert';
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 import './App.css';
 
 
 const App = () => (
+  <Provider store={store}>
   <Router>
     <Fragment>
     <Navbar/>
@@ -15,6 +21,7 @@ const App = () => (
           <Route exact path="/" element={<Landing />}/>
         </Routes>
         <section className='container'>
+          <Alert />
           <Routes>
             <Route exact path = "/register" element = {<Register/>} />
             <Route exact path = "/login" element = {<Login/>} />
@@ -22,6 +29,7 @@ const App = () => (
         </section>
     </Fragment>
   </Router> 
+  </Provider>
 );
 
 export default App;
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,10 @@
 import React, { Fragment, useState } from 'react';
-import axios from 'axios';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { setAlert } from '../../actions/alert';
+import PropTypes from 'prop-types'
 
-const Register = () => {
+const Register = ({ setAlert }) => {
     const [formData, setFormData] = useState({
         name: '',
         email: '',
@@ -17,26 +20,9 @@ const Register = () => {
     const onSubmit = async (e) => {
         e.preventDefault();
         if (password !== password2) {
-            console.log('Passwords do not match');
+            setAlert('Passwords do not match', 'danger');
         } else {
-            const newUser = {
-                name,
-                email,
-                password
-            }
-            try {
-                const config = {
-                    headers: {
-                        'Conten-Type': 'application/json; charset=UTF-8'
-                    }
-                }
-                const body = JSON.stringify(newUser);
-
-                const res = await axios.post('/api/users', body, config);
-                console.log(res.data);
-            } catch (err) {
-                console.error(err.message.data);
-            }
+            console.log('SUCCESS');
         }
     };
 
@@ -88,9 +74,13 @@ const Register = () => {
             </div>
             <input type="submit" value="Register" className="btn btn-primary" />
         </form>
-        <p className="my-1">Already have an account? <a href="login.html">Sign In</a></p>
+        <p className="my-1">Already have an account? <Link to="/login">Sign In</Link></p>
   </Fragment>
   )
 };
 
-export default Register;
+Register.propTypes = {
+    setAlert: PropTypes.func.isRequired
+}
+
+export default connect(null, { setAlert })(Register);
