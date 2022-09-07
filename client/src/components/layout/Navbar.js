fb3e8063f36d1a2ca04fc761d9255ee7c3a1233e
“r4dra” <rudrapratapsinghtomar@gmail.com>
2022-09-08 04:05:40 +0530

    Add Education and exp completed

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -7,13 +7,19 @@ import { logout } from '../../actions/auth';
 const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
     <ul>
-        <li>
-          <a onClick={logout} href="#!">
-            <i className= "fas fa-sign-out-alt"></i>{' '}
-            <span className="hide-sm">Logout</span>
-          </a>
-        </li>
-      </ul>
+      <li>
+        <Link to="/dashboard">
+          <i className= "fas fa-user"></i>{' '}
+          <span className="hide-sm">Dashboard</span>
+        </Link>
+      </li>
+      <li>
+        <a onClick={logout} href="#!">
+          <i className= "fas fa-sign-out-alt"></i>{' '}
+          <span className="hide-sm">Logout</span>
+        </a>
+      </li>
+    </ul>
   );
 
   const guestLinks = (
